4840373f2335187c3362e655820ce19496eb63c7
julio <ti43300@uvp.edu.mx>
2025-06-16 15:39:50 -0600

    quinto commit

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
@@ -7,9 +6,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyDu-_vpONpb42CSkPzjWwLZLlWkG0LLyXM",
   authDomain: "tiendabelleza-56ce2.firebaseapp.com",
   projectId: "tiendabelleza-56ce2",
-  storageBucket: "tiendabelleza-56ce2.appspot.com", // <-- corregido ".app" por ".com"
+  storageBucket: "tiendabelleza-56ce2.appspot.com",
   messagingSenderId: "78777047836",
-  appId: "1:78777047836:web:d08fea0926fe55c10a38d9"
+  appId: "1:78777047836:web:d08fea0926fe55c10a38d9",
+  databaseURL: "https://tiendabelleza-56ce2-default-rtdb.firebaseio.com"
 };
 
 const app = initializeApp(firebaseConfig);
